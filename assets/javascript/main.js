51328401623fe37053c081259a2938c17c591836
Jaime M <jaimemarsh19@gmail.com>
2023-06-03 20:50:06 -0400

    I was trying to view local storage from 'viewHighScores' button and made a mess, going to revert to before I did it and try again

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@
 let startButton = document.querySelector(".startButton")
 let timer = document.querySelector(".timer")
 let secondsLeft = 75;
-let startScreen = document.querySelector(".section");
+let startScreen = document.querySelector(".startScreen");
 
 //Quiz Questions
 let questionSection = document.querySelector(".questionSection");
@@ -12,44 +12,57 @@ let choices = document.querySelector(".choices");
 let answer = document.querySelector(".answers");
 let questionIndex = 0;
 
+//Final
+let finalScoreScreen = document.querySelector(".finalScoreScreen");;
+let finalScore = document.querySelector(".finalScore");
+let initials = document.querySelector("input");
+let submitBtn = document.querySelector(".submitBtn");
+
+//High Score
+let viewHighScoresBtn = document.querySelector(".viewHighScoresBtn");
+let highScoreScreen = document.querySelector(".highScoreScreen");
+let highScoreList = document.querySelector(".highScoreOl");
+let highScoreArrray = [];
+let backBtn = document.querySelector(".backBtn");
+let clearScoresBtn = document.querySelector(".clearScoresBtn");
+
 let questionsArray = [
-    [{
+    {
         title: "How is code sequenced?",
         choices: ['top to bottom', 'bottom to top', 'all at once', 'not at all'],
-        answer: '0',
+        answer: '1',
     },
     {
         title: "What is a loop?",
         choices: ['a mini program', 'code being executed repeatedly', 'a fast way to write a program'],
-        answer: '1',
+        answer: '2',
     },
     {
         title: "What is a conditional statement for?",
         choices: ['used to change a variable', 'used to redirect the flow of the program', 'shortens the length of your statement'],
-        answer: '1',
+        answer: '2',
     },
     {
         title: "What is a while loop",
         choices: ['Tests for a condition before the loop starts', 'tests for a condition afer the loop starts', 'tests for a condition repeatedly until it is met'],
-        answer: '0'
+        answer: '1'
     },
     {
         title: "What is a do.. while loop",
         choices: ['Tests for a condition before the loop starts', 'tests for a condition afer the loop starts', 'tests for a condition repeatedly until it is met'],
-        answer: '1'
+        answer: '2'
     },
     {
         title: "What is a for loop?",
         choices: ['Tests for a condition before the loop starts', 'tests for a condition afer the loop starts', 'tests for a condition repeatedly until it is met'],
-        answer: '2'
+        answer: '3'
     },
-    ]
 ]
 //timer
 function setTimer() {
     let timerInterval = setInterval(function () {
         secondsLeft--;
-        timer.textContern = secondsLeft;
+        timer.textContent = secondsLeft;
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
@@ -57,7 +70,7 @@ function setTimer() {
             endGame();
         } else if (questionIndex === questionsArray.length) {
             clearInterval(timerInterval)
-            endgame()
+            endGame()
         }
     }, 1000);
 };
@@ -66,7 +79,8 @@ function setTimer() {
 function startQuiz() {
     startScreen.setAttribute('class', 'hide');
     questionSection.removeAttribute('class', 'hide');
-
+    highScoreScreen.setAttribute("class", "hide");
+    finalScoreScreen.setAttribute("class", "hide");
     setTimer()
     runQuestions()
 }
@@ -74,8 +88,7 @@ function startQuiz() {
 //questions
 function runQuestions() {
     questions.textContent = questionsArray[questionIndex].title;
-    answer.textContent = ""
-        ;
+    answer.textContent = "";
     for (let i = 0; i < questionsArray[questionIndex].choices.length; i++) {
         let choiceBtn = document.createElement('button');
         choiceBtn.textContent = questionsArray[questionIndex].choices[i];
@@ -85,15 +98,95 @@ function runQuestions() {
     }
 }
 
-// //answers
-// function(checkAnswers)
+//answers
+function checkAnswers(event) {
+    let userAnswer = event.target.id;
+    if (userAnswer === questionsArray[questionIndex].answer) {
+        answer.textContent = "Correct";
+        secondsLeft += 10;
+    } else {
+        answer.textContent = "Incorrect";
+        secondsLeft -= 10;
+    }
+    questionIndex++;
+
+    if (questionIndex < questionsArray.length) {
+        runQuestions();
+    } else endGame();
+}
 
 
-// //endgame
-// function(endGame)
+//hide questions shows final screen
+function endGame() {
+    clearInterval(setTimer);
+    questionSection.setAttribute('class', 'hide');
+    finalScoreScreen.removeAttribute('class', 'hide');
+    finalScore.textContent = "Your final score is " + secondsLeft + "! Enter your initials here:";
+    finalScore.setAttribute('class', 'text is-family-code is-size-3')
 
-//hide questions and show final score
+}
+
+//backBtn
+backBtn.addEventListener("click", function () {
+    startScreen.removeAttribute('class', 'hide')
+    highScoreScreen.setAttribute('class', 'hide')
+})
 
 // Starts Quiz
 startButton.addEventListener("click", startQuiz);
 console.log("Quiz started");
+
+
+// submit button text
+submitBtn.addEventListener("click", function () {
+    document.getElementById("submitText").innerHTML = "Nice job! Click 'View High Score' to see your progress";
+});
+
+//view highscores, adding them to local storage
+function showingLocalStorage() {
+    // Get user's input
+    let initialsInput = document.querySelector(".initials").value;
+    showingLocalStorage();
+
+    // Create the highScore object
+    let highScore = {
+        initials: initialsInput,
+        timerCount: secondsLeft,
+    }
+
+    // Retrieve existing high scores from local storage
+    let storedHighScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    // Add the new high score to the array
+    storedHighScores.push(highScore);
+
+    // Store the updated high scores array in local storage
+    localStorage.setItem("highScores", JSON.stringify(storedHighScores));
+
+    // Clear the container to remove any previous content
+    highScoreList.innerHTML = "";
+
+    // Iterate over the high scores array and create HTML elements for each entry
+    storedHighScores.forEach(function (highScore) {
+        let highScoreElement = document.createElement("div");
+        highScoreElement.textContent = highScore.initials + " " + highScore.timerCount;
+        highScoreList.appendChild(highScoreElement);
+    });
+}
+showingLocalStorage()
+
+
+// clear high scores
+clearScoresBtn.addEventListener("click", function () {
+    localStorage.removeItem("highScores");
+    highScoreList.innerHTML = "";
+})
+
+// show high scores
+viewHighScoresBtn.addEventListener("click", function () {
+    showingLocalStorage()
+    highScoreScreen.removeAttribute('class', 'hide');
+    startScreen.setAttribute('class', 'hide');
+    finalScoreScreen.setAttribute('class', 'hide');
+
+})
